f01b0629a25f37c7573d24b0ebff5b1bd54a0f17
nejmal <lam.jennifer.ky@gmail.com>
2019-04-13 16:57:59 -0700

    Add data fetching in post.

---
+++ pages/post.js
@@ -0,0 +1,26 @@
+import { withRouter } from 'next/router';
+import fetch from 'isomorphic-unfetch';
+
+import Layout from '../components/Layout';
+
+const Post = ({ show }) => {
+  return (
+    <Layout>
+      <h1>{show.name}</h1>
+      <p>{show.summary.replace(/<[/]?p>/g, '')}</p>
+      <img src={show.image.medium} />
+    </Layout>
+  );
+};
+
+Post.getInitialProps = async context => {
+  const { id } = context.query;
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const show = await res.json();
+
+  console.log(`Fetched Show: ${show.name}`);
+
+  return { show };
+};
+
+export default withRouter(Post);
